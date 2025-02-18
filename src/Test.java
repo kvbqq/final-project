cec1685ef061092c5e7eedb7e1a4f0679119714a
kvbqq <kvbqqq@gmail.com>
2025-02-18 20:54:04 +0100

    task-3
    (cherry picked from commit 3501213d754495ae2174bcbb8d7bedd585d8d6e5)

--- src/Test.java
+++ src/Test.java
@@ -1,8 +1,14 @@
+import models.Cart;
 import services.ProductManager;
 
 public class Test {
     public static void main(String[] args) {
         ProductManager productManager = new ProductManager();
         productManager.displayProducts();
+
+        Cart cart = new Cart();
+//        cart.addToCart(ProductManager.getProducts().get(0));
+
+//        System.out.println(cart);
     }
 }
---
+++ src/models/Cart.java
@@ -0,0 +1,12 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Cart {
+    private List<Product> products = new ArrayList<>();
+
+
+}
