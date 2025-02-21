511b28562d6c489b6d52e1e7fdb12c75f20ba10b
kvbqq <kvbqqq@gmail.com>
2025-02-21 12:40:20 +0100

    task-1
    (cherry picked from commit 3bb08863fc7ef56f4ee06de1ffc623d49e0e0056)

--- src/models/Product.java
+++ src/models/Product.java
@@ -43,37 +43,41 @@ public class Product {
         return stock;
     }
 
+    public List<Configuration> getConfigurations() {
+        return configurations;
+    }
+
     @Override
     public String toString() {
         return String.format("%d | %s | %s | %.2f zł", id, name, type.getName(), price);
     }
 
-    public Optional<List<Configuration>> configure() {
-        List<Configuration> finalConfiguration = new ArrayList<>();
-
-        Set<ConfigurationType> configurationTypeSet = configurations.stream()
-                .map(Configuration::getType)
-                .collect(Collectors.toSet());
-
-        for (ConfigurationType configurationType : configurationTypeSet) {
-            System.out.println("Konfiguracja elementu - " + configurationType.getName());
-            List<Configuration> configurationList = configurations.stream()
-                    .filter(configuration -> configuration.getType().equals(configurationType))
-                    .toList();
-
-            configurationList.stream()
-                    .map(configuration -> String.format(
-                            "[%d] %s | Dopłata: %.2f zł",
-                            configurationList.indexOf(configuration)+1,
-                            configuration.getName(),
-                            configuration.getPrice()
-                        )
-                    )
-                    .forEach(System.out::println);
-
-            System.out.println("\nWybierz jedną z dostępnych opcji:");
-            // TODO - read single value, read multiple values
-        }
-        return Optional.of(finalConfiguration);
-    }
+//    public Optional<List<Configuration>> configure() {
+//        List<Configuration> finalConfiguration = new ArrayList<>();
+//
+//        Set<ConfigurationType> configurationTypeSet = configurations.stream()
+//                .map(Configuration::getType)
+//                .collect(Collectors.toSet());
+//
+//        for (ConfigurationType configurationType : configurationTypeSet) {
+//            System.out.println("Konfiguracja elementu - " + configurationType.getName());
+//            List<Configuration> configurationList = configurations.stream()
+//                    .filter(configuration -> configuration.getType().equals(configurationType))
+//                    .toList();
+//
+//            configurationList.stream()
+//                    .map(configuration -> String.format(
+//                            "[%d] %s | Dopłata: %.2f zł",
+//                            configurationList.indexOf(configuration)+1,
+//                            configuration.getName(),
+//                            configuration.getPrice()
+//                        )
+//                    )
+//                    .forEach(System.out::println);
+//
+//            System.out.println("\nWybierz jedną z dostępnych opcji:");
+//            // TODO - read single value, read multiple values
+//        }
+//        return Optional.of(finalConfiguration);
+//    }
 }
