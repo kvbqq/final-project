539212cccd1f1714581af49f7739067ecffdd20a
kvbqq <kvbqqq@gmail.com>
2025-02-22 17:42:57 +0100

    Revert "task-5"
    This reverts commit 146774672af5f21aaaa079664c6583199bda3ddf.

--- src/models/ConfigurationType.java
+++ src/models/ConfigurationType.java
@@ -7,12 +7,7 @@ public enum ConfigurationType {
     BATTERY_CAPACITY("Pojemność baterii"),
     SMARTPHONE_ACCESSORY("Akcesoria");
 
-    // new enum - type of configuration (multiple/single)
-
     private final String name;
-
-    // configurationType based on enum (Single/Multiple)
-
 
     ConfigurationType(String name) {
         this.name = name;
